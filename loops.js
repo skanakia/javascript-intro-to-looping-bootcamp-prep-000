ce8ec2fe2d18eceecd0480869fb58c5c8d8d1039
Sameer Kanakia <skanakia1@gmail.com>
2018-03-23 09:53:39 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -1,10 +1,10 @@
 function forLoop(array) {
   for (let i = 0; i < 26; i++) {
     if(i===1) {
-      var y = x + "I am 1 strange loop. "
-      return y
+      array = "I am 1 strange loop. "
+      return array
     } else {
-      y = y + "I am ${i} strange loops. "
+      array = array + "I am ${i} strange loops. "
       return y 
     }
   }
