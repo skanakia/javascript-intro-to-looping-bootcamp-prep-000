551f2e8fade36829b67e29d7cb8662be8edb6e81
Sameer Kanakia <skanakia1@gmail.com>
2018-03-23 09:51:21 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -2,7 +2,9 @@ x = null
 for (let i = 0; i < 26; i++) {
   if(i===1) {
     var y = x + "I am 1 strange loop. "
+    return y
   } else {
     y = y + "I am ${i} strange loops. "
+    return y 
   }
 }
